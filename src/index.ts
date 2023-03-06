3360056b2c030aca24dabb1c9151e99115572fe4
yogesh <ayezinzu@gmail.com>
2023-03-06 18:00:22 +0530

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import { greetUser } from '$utils/greet';
 
+import type { CMSList } from './types/CMSList';
+
 window.Webflow ||= [];
 window.Webflow.push(() => {
   const name = 'John Doe';
   greetUser(name);
 });
+window.fsAttributes = window.fsAttributes || [];
+window.fsAttributes.push([
+  'cmsload',
+  (listInstances: CMSList[]) => {
+    const [listInstance] = listInstances;
+    console.log(listInstance);
+    const [item] = listInstance.items;
+    const itemTemplateElement = item.element;
+  },
+]);
